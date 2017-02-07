8ed39448e68e32479974300ecbb8f7bc9448aa8b
Joyce Ho <joyceho@utexas.edu>
2017-02-06 20:37:46 -0500

    Adding lectures and examples for 2/7 lectures

---
+++ cs377_s17/script/company-ex-1.sql
@@ -0,0 +1,19 @@
+USE companyDB;
+
+/* Retrieve the birthdate and address of the employee whose name is ‘John B. Smith” */
+SELECT bdate, address
+FROM employee
+WHERE fname = 'John' AND minit='B' AND lname='Smith';
+
+/* List the SSN, last name, and department number of all employees */
+SELECT ssn, lname, dno
+FROM employee;
+
+/* List the department number and name of all departments */
+SELECT dnumber, dname
+FROM department;
+
+/* List the projects under department number 5 */
+SELECT *
+FROM projects
+WHERE dnumber = 5;
